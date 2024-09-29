3d8f12e0f23b41e36f630e1a3c1edfe5d2fd173e
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-09-29 13:44:58 +0200

    Add slimevr openvr driver package
    Signed-off-by: C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>

---
+++ packages/slime_openvr_driver.nix
@@ -0,0 +1,69 @@
+ { stdenv, lib
+, fetchurl
+, fetchFromGitHub
+, alsaLib
+, openssl
+, zlib
+, pulseaudio
+, autoPatchelfHook
+, pkg-config
+, cmake
+, gnumake
+, vcpkg
+, protobuf
+, simdjson
+, openvr
+}:
+
+stdenv.mkDerivation rec {
+  pname = "slimevr-openvr-driver";
+  version = "0.2.2";
+
+  src = fetchFromGitHub {
+    owner = "SlimeVR";
+    repo = "SlimeVR-OpenVR-Driver";
+    fetchSubmodules = true;
+    rev = "v" + version;
+    hash = "sha256-XyzJWmlcCZzymw+7wrmQNSrssY9BxDY5lygnZHGAh1o=";
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+    cmake
+    vcpkg
+    gnumake
+    autoPatchelfHook
+  ];
+
+  buildInputs = [
+     openvr
+     protobuf
+     simdjson
+#    alsaLib
+#    openssl
+#    zlib
+#    pulseaudio
+  ];
+
+  env = {
+    VCPKG_ROOT = "${vcpkg}/share/vcpkg";
+  };
+
+  cmakeFlags = [
+    "-DVCPKG_MANIFEST_INSTALL=OFF"
+    "-DOPENVR_LIB=${openvr}/lib"
+    "-DVCPKG_PATH=${vcpkg}/bin/vcpkg"
+  ];
+
+  installPhase = ''
+    runHook preInstall
+    cp -r driver/slimevr $out/
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    homepage = "https://slimevr.dev";
+    description = "SlimeVR driver for OpenVR";
+    platforms = platforms.linux;
+  };
+}
