6c2dabb03ffd890d151e2b12cae3bdb43abe38cd
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-10-09 11:37:22 +0200

    Fix package paths
    Signed-off-by: C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>

--- packages.nix
+++ packages.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}:
 {
-  slimevr_feeder = pkgs.callPackage ./slimevr_feeder.nix {};
-  slimevr_openvr_driver = pkgs.callPackage ./slimevr_openvr_driver.nix {};
-  slimevr-server-gui = pkgs.callPackage ./slimevr-server-gui {};
+  slimevr_feeder = pkgs.callPackage ./packages/slimevr_feeder.nix {};
+  slimevr_openvr_driver = pkgs.callPackage ./packages/slimevr_openvr_driver.nix {};
+  slimevr-server-gui = pkgs.callPackage ./packages/slimevr-server-gui {};
 }
