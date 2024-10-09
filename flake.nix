a12ee72682c26fda0b83a28ae0cd9a5ef0a832c5
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-10-09 11:21:51 +0200

    Try to add flake
    Signed-off-by: C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Modules for SlimeVR on NixOs";
+
+  inputs = {
+#    flake-parts.url = "github:hercules-ci/flake-parts";
+#    utils.url = "github:numtide/flake-utils";
+#    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-compat = {
+      url = github:edolstra/flake-compat;
+      flake = true;
+    };
+#    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+#    treefmt-nix.url = "github:numtide/treefmt-nix";
+#    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs@{ ... }:
+    {
+      nixosModules.default = {
+        imports = [
+          ./packages.nix
+        ];
+      };
+    };
+}
---
+++ packages.nix
@@ -0,0 +1,5 @@
+{
+  slimevr_feeder = callPackage ./slimevr_feeder.nix {};
+  slimevr_openvr_driver = callPackage ./slimevr_openvr_driver.nix {};
+  slimevr-server-gui = callPackage ./slimevr-server-gui {};
+}
