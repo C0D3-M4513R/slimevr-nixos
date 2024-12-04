3faffe88b3a5c8d42633faf98b75b2566eed89ff
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-12-04 09:15:38 +0100

    Update SlimeVr Server to 0.13.2
    Signed-off-by: C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>

--- packages/slimevr-server/default.nix
+++ packages/slimevr-server/default.nix
@@ -3,6 +3,8 @@ stdenv
 , lib
 , fetchFromGitHub
 , fetchurl
+, fetchzip
+
 , autoPatchelfHook
 , makeBinaryWrapper
 , wrapGAppsHook
@@ -32,55 +34,33 @@ stdenv
 
 rustPlatform.buildRustPackage rec {
   pname = "slimevr-server";
-  version = "0.13.0";
+  version = "0.13.2";
 
   src = fetchFromGitHub {
     owner = "SlimeVR";
     repo = "SlimeVR-Server";
     fetchSubmodules = true;
     rev = "v" + version;
-    hash = "sha256-Q2N+oYQ1VgJ+XyBRhCPY4YVSNP6kiZ6ErPHS13XeVrE=";
-   # populate values that require us to use git. By doing this in postFetch we
-   # can delete .git afterwards and maintain better reproducibility of the src.
-   leaveDotGit = true;
-    postFetch = ''
-      cd "$out"
-      HASH=$(git rev-parse --verify --short HEAD)
-      substituteInPlace $out/gui/vite.config.ts \
-        --replace-fail "execSync('git rev-parse --verify --short HEAD')" "\"$HEAD\"" \
-        --replace-fail "const versionTag = execSync('git --no-pager tag --sort -taggerdate --points-at HEAD')
-  .toString()
-  .split('\n')[0]
-  .trim();
-" "const versionTag = \"v${version}\";" \
-       --replace-fail "const gitClean = execSync('git status --porcelain').toString() ? false : true;" "const gitClean = true"
-
-      substituteInPlace $out/gui/src-tauri/src/util.rs \
-        --replace-fail "const VERSION: &str = if build::TAG.is_empty() {
-	build::SHORT_COMMIT
-} else {
-	build::TAG
-};" "const VERSION: &str = \"v${version}\";" \
-        --replace-fail 'const MODIFIED: &str = if build::GIT_CLEAN { "" } else { "-dirty" };' 'const MODIFIED: &str = "";' 
-
-      find "$out" -name .git -print0 | xargs -0 rm -rf
-    '';
+    hash = "sha256-XQDbP+LO/brpl7viSxuV3H4ALN0yIkj9lwr5eS1txNs";
   };
 
-  cargoHash = "sha256-feIEKCKFvIUns5ttswdclMv0PCIHFSzSAtPum5IzYX8=";
+  cargoHash = "sha256-hCI0IQpGgSO7dXdz3gXXBGStOTlZNIXdkPhq1wiUxFo=";
 
   jar = fetchurl {
     url = "https://github.com/SlimeVR/SlimeVR-Server/releases/download/v${version}/slimevr.jar";
-    hash = "sha256-Sj39D/QAR/lwojxsRcpe0k/21HC6R9mHPrj+H6s+qZA=";
+    hash = "sha256-s6uznJtsa1bQAM1QIdBMey+m9Q/v2OfKQPXjD5RAR78=";
+  };
+
+  guiHtml = fetchzip {
+    url = "https://github.com/SlimeVR/SlimeVR-Server/releases/download/v${version}/slimevr-gui-dist.tar.gz";
+    stripRoot = false;
+    hash = "sha256-u1EtSG1uTGIyROo5TznzQ+Z8g6WAiU0SmDObm/w1f9k=";
   };
 
   nativeBuildInputs = [
-    nodejs
-    pnpm.configHook
 #required things for building
     cargo
     rustc
-    husky
     pkg-config
     makeBinaryWrapper
     autoPatchelfHook
@@ -91,34 +71,19 @@ rustPlatform.buildRustPackage rec {
     glib
     gtk3.dev
     webkitgtk_4_1.dev
-
-    gobject-introspection
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
   ];
 
-  pnpmDeps = pnpm.fetchDeps {
-    inherit pname version src;
-    hash = "sha256-L5ndNzESKddcQCLugmSQyvWr6QVKrMmR1qiclYPzhto";
-  };
-
   preBuild = ''
-    pnpm i
     pushd gui
-    pnpm run build
+    cp -r ${guiHtml} dist
     popd
   '';
 
   preInstall = ''
-    mkdir -p $out/jar
-    cp ${jar} $out/jar/slimevr.jar
-    mkdir -p $out/share/applications
-    mkdir -p $out/icon
-    cp ${src}/gui/src-tauri/icons/icon.svg $out/icon
-    cp ${src}/gui/src-tauri/dev.slimevr.SlimeVR.desktop $out/share/applications/
-    sed -i $out/share/applications/dev.slimevr.SlimeVR.desktop -e "s|{{exec}}|$out/bin/slimevr|" -e "s|{{icon}}|$out/icon/icon.svg|"
+    install -Dm644 ${jar} $out/share/slimevr/slimevr.jar
+    install -Dm644 ${src}/gui/src-tauri/icons/icon.svg $out/share/icons/hicolor/scalable/apps/slimevr.svg
+    install -Dm644 ${src}/gui/src-tauri/dev.slimevr.SlimeVR.desktop $out/share/applications/dev.slimevr.SlimeVR.desktop
+    sed -i $out/share/applications/dev.slimevr.SlimeVR.desktop -e "s|{{exec}}|$out/bin/slimevr|" -e "s|{{icon}}|slimevr|"
   '';
 
   runtimeDependencies = [
